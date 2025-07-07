1336c4550b12465860c53280c0c3d2471bc6902d
Asma <183721570+asmamulla786@users.noreply.github.com>
2025-07-07 14:19:26 +0530

    replaced in memory database with postgresql

--- src/main/java/com/example/mytodo/controller/TodoController.java
+++ src/main/java/com/example/mytodo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.mytodo.controller;
 
 import com.example.mytodo.model.Todo;
 import com.example.mytodo.repository.TodoRepository;
+import com.example.mytodo.service.TodoService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,25 +10,23 @@ import java.util.List;
 
 @RestController
 public class TodoController {
-    private final TodoRepository todoRepository;
-    public TodoController(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
+    private TodoService todoService;
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
     }
 
     @GetMapping("/todos")
     public List<Todo> getTodos(){
-        return todoRepository.findAll();
+        return todoService.getTodos();
     }
 
     @PostMapping("/todos")
-    public Todo postTodos(@Valid @RequestBody Todo todo){
-        System.out.println("todo "+todo);
-        todoRepository.save(todo);
-        return todo;
+    public Todo addTodo(@Valid @RequestBody Todo todo){
+        return todoService.addTodo(todo);
     }
+
     @DeleteMapping("/todos/{id}")
-    public String deleteTodos(@PathVariable Long id){
-        todoRepository.deleteById(id);
-        return "deleted" + id + "todo";
+    public String deleteTodo(@PathVariable Long id){
+        return todoService.deleteTodo(id);
     }
 }
--- src/main/java/com/example/mytodo/service/TodoService.java
+++ src/main/java/com/example/mytodo/service/TodoService.java
@@ -1,8 +1,29 @@
 package com.example.mytodo.service;
 
+import com.example.mytodo.model.Todo;
+import com.example.mytodo.repository.TodoRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Service
 public class TodoService {
+    private final TodoRepository todoRepository;
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+    public List<Todo> getTodos(){
+        return todoRepository.findAll();
+    }
 
+    public Todo addTodo(@Valid @RequestBody Todo todo){
+        todoRepository.save(todo);
+        return todo;
+    }
+    public String deleteTodo(@PathVariable Long id){
+        todoRepository.deleteById(id);
+        return "deleted " + id + " todo";
+    }
 }
